3285a107ebf550ee8095defb8b87787e3368a50b
EdricJensenSee <edric_see@dlsu.edu.ph>
2023-03-07 21:55:23 +0800

    Problems relating to the questions were fixed

--- SeeE.c
+++ SeeE.c
@@ -25,32 +25,38 @@ void providedRecords (struct records *cRecords){
 	strcpy (cRecords[1].cChoice3, "Thanos");	
 	
 	strcpy (cRecords[2].fTopic1, "Literature");
-	strcpy (cRecords[2].cQuestion1, "In the movie named 1984, what is Winston's apartment called?)";
-	strcpy (cRecords[2].cChoice1, "Oceania View)";
-	strcpy (cRecords[2].cChoice2, "London House)";
-	strcpy (cRecords[2].cChoice3, "Victory Mansions)";
+	strcpy (cRecords[2].cQuestion1, "In the movie named 1984, what is Winston's apartment called?");
+	strcpy (cRecords[2].cChoice1, "Oceania View");
+	strcpy (cRecords[2].cChoice2, "London House");
+	strcpy (cRecords[2].cChoice3, "Victory Mansions");
 	
 }
 
 
 int menu (){
-int nChoice;
+int nChoice, nInvalid = 1;
 printf("[1] Manage Data as an admin\[2] Play as a player\n[3) Exit Program\n\nInput your choice: ");
 scanf("%d",&nChoice);
-while (nChoice <1 || nChoice>3){
+if (nChoice == 1 && nChoice == 2 && nChoice == 3)
+	nInvalid = 0;
+while (nInvalid=1){
 	printf("Input valid input");
 	scanf("%d", &nChoice);
+	
+if (nChoice == 1 && nChoice == 2 && nChoice == 3)
+	nInvalid = 0;
 }
 system("cls");
 return nChoice;
 }
 
 int pPassValid (){
-	char cPass[11] = "", cKey[10] = "AdminPass", cAdd[11];
+	char cPass[11], cKey[10] = "AdminPass", cAdd[11];
 	int i, j, nAsterisks = 0;
+	cPass[0] = '\0';
 	while (strcmp(cKey,cPass) != 0) {
 		nAsterisks = strlen(cPass);
-		printf("******Manage Data******");
+		printf("*****Manage Data*****");
 		printf("\nInput Password (9): ");
 		for (j=0; j<nAsterisks; j++)
 			printf("*");
@@ -86,7 +92,7 @@ void pPlay(){
 int main (){
 struct records cRecords[20];
 if (menu() == 1)
-mData(&cRecords);
+mData(&cRecords[]);
 else if (menu() == 2)
 pPlay();
 else if (menu() == 3)
